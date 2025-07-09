ee17f313e2bd217f2e4548ff3f6f2a51afbd2808
Alexandro Alonso <159957257+AlexAlonso002@users.noreply.github.com>
2025-07-09 11:35:44 -0500

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -6,9 +6,6 @@ const cors = require('cors');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
